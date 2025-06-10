521910c95d3f723cac7dae080e66f31701c1ed80
kanishka-kumar <kanishkakumar23@gmail.com>
2025-06-10 20:25:46 +0530

    Using AnnotationConfigApplicationContext for using @Bean and @Configuration

--- src/main/java/com/example/main/Example1.java
+++ src/main/java/com/example/main/Example1.java
@@ -1,11 +1,17 @@
 package com.example.main;
 
 import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Example1 {
     public static void main(String[] args) {
         Vehicle vehicle = new Vehicle();
         vehicle.setName("Honda Amaze...");
         System.out.println("Vehicle name from Non-Spring Context : " + vehicle.getName());
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context : " + veh.getName());
     }
 }
